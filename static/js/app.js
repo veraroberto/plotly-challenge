5a969c204df1a06b6d4c45f2e826567c387c5564
Roberto Vera <robertovera89@gmail.com>
2021-07-04 17:02:21 -0500

    Creates the function to generates the Demo Info and the Bar Chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // const url = "http://robdunnlab.com/projects/belly-button-biodiversity/";
 
 
-var SubjectID = d3.selectAll("#selDataset");
+var SubjectID = d3.select("#selDataset");
 var DemoInfo = d3.select("#sample-metadata");
 var barGraph = d3.select("#bar");
 var gaugeGraph = d3.select("#gauge");
@@ -19,7 +19,7 @@ importJSON.then((incomingData) => {
     })
 })
 
-// Creates the Demograpgic Info
+// Creates the Demograpgic Info Panel 
 function createDemoInfo(metadata) {
     DemoInfo.html("");
 
@@ -32,127 +32,68 @@ function createDemoInfo(metadata) {
 
 };
 
-
+function sortSamples(a, b) {
+    return a.sample_sample_values - b.sample_sample_values;
+}
 
 // Creates the Defaul Demographic Info in the Panel
-importJSON.then((incomingData) => {
-    var idsNames = incomingData.names;
-    var metadata = incomingData.metadata;
-    var samples = incomingData.samples;
-
-    var noID = parseInt(SubjectID.property('value'));
-
-    var metadataDemoInfo = metadata.find((metadata) => metadata.id === noID);
-    createDemoInfo(metadataDemoInfo)
-
-
-    var countIDS = idsNames.map(x => [x, 0]);
-    samples.forEach(sampleElement => {
-        sampleElement['otu_ids'].forEach(otu_idsElement => {
-            // console.log(otu_idsElement)
-            countIDS.forEach(ids => {
-                // console.log(typeof ids[0])
-                // console.log(typeof otu_idsElement)
-                if (otu_idsElement === parseInt(ids[0])) {
-                    ids[1] = ids[1] + 1;
-
-                }
-            })
-        })
-    })
-
-
-
-    var sortedcountIDS = countIDS.sort(function sortFunction(a, b) {
-        return b[1] - a[1];
-    });
-    // console.log(sortedcountIDS)
-
-    xvalues = [];
-    yvalues = [];
-
-    sortedcountIDS.forEach(count => {
-        // console.log(count[0])
-        xvalues.push(count[0])
-        yvalues.push(count[1])
-    })
-
-    console.log(xvalues.slice(0, 10));
-    console.log(yvalues.slice(0, 10));
-    var trace = {
-        type: 'bar',
-        orientation: 'h',
-        x: xvalues.slice(0, 10),
-        y: yvalues.slice(0, 10),
-    };
-
-    var layout = {
-        title: {
-            text: "Top 10 Bacteria Cultures Found",
-        },
-        xaxis: {
-            title: {
-                text: "Sample Values",
-                standoff: 15
-            }
-        }
-    };
-
-    Plotly.newPlot('bar', [trace], layout);
-});
-
-
-function optionChanged() {
-    var noID = parseInt(SubjectID.property('value'));
-    // console.log(typeof noID)
+function createGraphs() {
     importJSON.then((incomingData) => {
         var idsNames = incomingData.names;
         var metadata = incomingData.metadata;
         var samples = incomingData.samples;
 
+        var noID = parseInt(SubjectID.property('value'));
 
         var metadataDemoInfo = metadata.find((metadata) => metadata.id === noID);
         createDemoInfo(metadataDemoInfo)
 
+        var samplesDemoInfo = samples.find((samples) => parseInt(samples.id) === noID);
+        console.log(typeof samplesDemoInfo);
 
-    })
+        //Separtes the differnet info in differents array
+        var sampleID = samplesDemoInfo.id;
+        var sample_otu_ids = samplesDemoInfo.otu_ids;
+        var sample_otu_lables = samplesDemoInfo.otu_labels;
+        var sample_sample_values = samplesDemoInfo.sample_values;
+
+
+        var trace = {
+            type: 'bar',
+            orientation: 'h',
+            x: sample_sample_values.slice(0, 10).reverse(),
+            y: sample_otu_ids.slice(0, 10).map((OTU) => `OTU ${String(OTU)} `).reverse(),
+            text: sample_otu_lables.slice(0, 10).slice(0, 10)
+        };
+
+        var layout = {
+            title: {
+                text: `Top 10 Bacteria Cultures Found in ID No.: ${sampleID}`
+            },
+            xaxis: {
+                title: {
+                    text: "Sample Values",
+                    standoff: 15
+                }
+            }
+        };
+
+        Plotly.newPlot('bar', [trace], layout);
+
+
+    });
 
 };
 
-// // Use d3.json() to fetch data from JSON file
-// // Incoming data is internally referred to as incomingData
-// d3.json("samples.json").then((incomingData) => {
-//     var idsNames = incomingData.names;
-//     var metadata = incomingData.metadata;
-//     var samples = incomingData.samples;
+//Calls the Function for the Default Values
+createGraphs();
 
-//     // console.log(idsNames);
-//     // console.log(metadata);
-//     // console.log(samples);
-//     var countIDS = idsNames.map(x => [x, 0]);
+//Function for optionChanged
+function optionChanged() {
 
+    createGraphs();
 
-//     samples.forEach(sampleElement => {
-//         sampleElement['otu_ids'].forEach(otu_idsElement => {
-//             // console.log(otu_idsElement)
-//             countIDS.forEach(ids => {
-//                 // console.log(typeof ids[0])
-//                 // console.log(typeof otu_idsElement)
-//                 if (otu_idsElement === parseInt(ids[0])) {
-//                     ids[1] = ids[1] + 1;
-
-//                 }
-//             })
-//         })
-//     })
-
-
-
-//     var sortedcountIDS = countIDS.sort(function sortFunction(a, b) {
-//         return b[1] - a[1];
-//     });
-//     // console.log(sortedcountIDS)
-// });
+};
 
 
 
