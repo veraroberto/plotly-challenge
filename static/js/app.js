1574121a28350a15d69076f930f9147b9c9ac269
Roberto Vera <robertovera89@gmail.com>
2021-07-04 21:25:44 -0500

    Belly Button gauge

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ var SubjectID = d3.select("#selDataset");
 var DemoInfo = d3.select("#sample-metadata");
 var barGraph = d3.select("#bar");
 var gaugeGraph = d3.select("#gauge");
-var bubleGraph = d3.select("#bubble");
+var bubleGraph = d3.select("#myDiv");
 
 var importJSON = d3.json("samples.json");
 
@@ -32,10 +32,6 @@ function createDemoInfo(metadata) {
 
 };
 
-function sortSamples(a, b) {
-    return a.sample_sample_values - b.sample_sample_values;
-}
-
 // Creates the Defaul Demographic Info in the Panel
 function createGraphs() {
     importJSON.then((incomingData) => {
@@ -46,10 +42,10 @@ function createGraphs() {
         var noID = parseInt(SubjectID.property('value'));
 
         var metadataDemoInfo = metadata.find((metadata) => metadata.id === noID);
+
         createDemoInfo(metadataDemoInfo)
 
         var samplesDemoInfo = samples.find((samples) => parseInt(samples.id) === noID);
-        console.log(typeof samplesDemoInfo);
 
         //Separtes the differnet info in differents array
         var sampleID = samplesDemoInfo.id;
@@ -57,8 +53,9 @@ function createGraphs() {
         var sample_otu_lables = samplesDemoInfo.otu_labels;
         var sample_sample_values = samplesDemoInfo.sample_values;
 
+        //Creates the Bar Chart
 
-        var trace = {
+        var traceBar = {
             type: 'bar',
             orientation: 'h',
             x: sample_sample_values.slice(0, 10).reverse(),
@@ -66,9 +63,9 @@ function createGraphs() {
             text: sample_otu_lables.slice(0, 10).slice(0, 10)
         };
 
-        var layout = {
+        var layoutBar = {
             title: {
-                text: `Top 10 Bacteria Cultures Found in ID No.: ${sampleID}`
+                text: `Top 10 Bacteria Cultures Found`
             },
             xaxis: {
                 title: {
@@ -78,7 +75,78 @@ function createGraphs() {
             }
         };
 
-        Plotly.newPlot('bar', [trace], layout);
+        Plotly.newPlot('bar', [traceBar], layoutBar);
+
+        //Creates the Bubble Charts
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size.
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text values.
+        // To scale the bubble size, use the attribute sizeref. We recommend using the following formula to calculate a sizeref value:
+        // sizeref = 2.0 * Math.max(...size) / (desired_maximum_marker_size**2)
+        // Note that setting 'sizeref' to a value greater than 1, decreases the rendered marker sizes, while setting 'sizeref' to less than 1, increases the rendered marker sizes. See https://plotly.com/python/reference/scatter/#scatter-marker-sizeref for more information. Additionally, we recommend setting the sizemode attribute: https://plotly.com/python/reference/scatter/#scatter-marker-sizemode to area.
+
+        var traceBubble = {
+            x: sample_otu_ids,
+            y: sample_sample_values,
+            text: sample_otu_lables,
+            mode: 'markers',
+            marker: {
+                size: sample_otu_ids,
+                color: sample_otu_ids,
+                sizemode: 'area'
+            }
+        };
+
+
+        var data = [traceBubble];//, trace2, trace3, trace4];
+
+        var layoutBuble = {
+            title: 'Bacteria Cultures Per Sample',
+            showlegend: false,
+            xaxis: {
+                title: `OTU ID: ${sampleID}`
+            }
+
+
+        };
+
+        Plotly.newPlot('buble', data, layoutBuble);
+
+
+
+        //Create the Gauge 
+        var traceGauge = [
+            {
+                //   domain: { x: [0, 1], y: [0, 1] },
+                value: metadataDemoInfo.wfreq,
+                title: { text: "Belly Button Washing Frequency" },
+                type: "indicator",
+                mode: "gauge+number",
+   
+                gauge: {
+                    axis: { range: [0, 9] },
+                    steps: [
+                        { range: [0, 1], color: "#B0E0E6" },
+                        { range: [1, 2], color: "#5F9EA0" },
+                        { range: [2, 3], color: "#4682B4" },
+                        { range: [3, 4], color: "#6495ED" },
+                        { range: [4, 5], color: "#00BFFF" },
+                        { range: [5, 6], color: "#ADD8E6" },
+                        { range: [6, 7], color: "#1E90FF" },
+                        { range: [7, 8], color: "#4169E1" },
+                        { range: [8, 9], color: "#00008B" },
+
+                    ],
+
+                }
+            }
+        ];
+
+        var layoutGauge = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', traceGauge, layoutGauge);
+
 
 
     });
